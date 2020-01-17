44a2562b022ab01007196035f719bedf2ab130bd
Changrui Zhou <changruizzzz@gmail.com>
2020-01-17 14:43:53 -0500

    Updates

---
+++ precache-manifest.097364a663d7a8c36bf146689bbd73bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "581ae0760d4df5066848ee1deda6a03b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3f0e839bf0a6d249501e",
+    "url": "/monsters-rolodex/static/css/main.235c63bc.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3f0e839bf0a6d249501e",
+    "url": "/monsters-rolodex/static/js/main.7c51a79c.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
